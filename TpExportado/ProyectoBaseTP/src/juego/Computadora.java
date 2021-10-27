e09db389288c9dda8fd8e35585dabd834b04b60e
Facundonah01 <93155867+Facundonah01@users.noreply.github.com>
2021-10-26 23:54:06 -0300

    Add files via upload

---
+++ TpExportado/ProyectoBaseTP/src/juego/Computadora.java
@@ -0,0 +1,23 @@
+package juego;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
+import java.awt.*;
+
+public class Computadora {
+
+    private int x, y;
+    private int ancho, alto;
+
+    public Computadora(int x, int y, int ancho, int alto) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public void dibujarse(Entorno entorno){
+        entorno.dibujarImagen(Herramientas.cargarImagen("imagenes/computer.png"), this.x, this.y, 0);
+    }
+}
